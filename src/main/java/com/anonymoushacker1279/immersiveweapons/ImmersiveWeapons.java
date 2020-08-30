57cf784cb030d394da9dbc32e7549180334c2f77
Anonymous Hacker <48810167+AnonymousHacker1279@users.noreply.github.com>
2020-08-30 16:17:49 -0400

    Update to MC 1.16.2
    The mod now works with Minecraft 1.16.2. The mod has been compiled with Forge 1.16.2-33.0.21.
    
    Due to the early development stages of 1.16.2 Forge, ore/structure generation has not yet been implemented. The abandoned factory structure will not spawn naturally in the world, and ores will not generate. This functionality will be re-implemented once Forge updates.

--- src/main/java/com/anonymoushacker1279/immersiveweapons/ImmersiveWeapons.java
+++ src/main/java/com/anonymoushacker1279/immersiveweapons/ImmersiveWeapons.java
@@ -7,47 +7,22 @@ import com.anonymoushacker1279.immersiveweapons.util.DeferredRegistryHandler;
 
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.Biomes;
-import net.minecraft.world.gen.GenerationStage;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.IFeatureConfig;
-import net.minecraft.world.gen.feature.NoFeatureConfig;
-import net.minecraft.world.gen.feature.OreFeatureConfig;
-import net.minecraft.world.gen.feature.OreFeatureConfig.FillerBlockType;
-import net.minecraft.world.gen.feature.structure.IStructurePieceType;
-import net.minecraft.world.gen.feature.structure.Structure;
-import net.minecraft.world.gen.placement.ConfiguredPlacement;
-import net.minecraft.world.gen.placement.CountRangeConfig;
-import net.minecraft.world.gen.placement.IPlacementConfig;
-import net.minecraft.world.gen.placement.Placement;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.DeferredWorkQueue;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.ObjectHolder;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@SuppressWarnings("deprecation")
 @Mod("immersiveweapons")
 @Mod.EventBusSubscriber(modid = ImmersiveWeapons.MOD_ID, bus = Bus.MOD)
 public class ImmersiveWeapons
 {
 	
 	public static final String MOD_ID = "immersiveweapons";
-	public static final ResourceLocation ABANDONED_FACTORY_LOCATION = new ResourceLocation(ImmersiveWeapons.MOD_ID, "abandoned_factory");
-	
-	public static IStructurePieceType ABANDONED_FACTORY_PIECE = null;
-	
-	@ObjectHolder(ImmersiveWeapons.MOD_ID+":abandoned_factory")
-	public static Structure<NoFeatureConfig> ABANDONED_FACTORY;
 	
 	// Setup logger
 	public static final Logger LOGGER = LogManager.getLogger();
@@ -66,43 +41,7 @@ public class ImmersiveWeapons
 
     
     private void setup(final FMLCommonSetupEvent event) {
-    	DeferredWorkQueue.runLater(new Runnable() {
-    		@Override
-    		public void run() {
-    			for(Biome biome : ForgeRegistries.BIOMES) {
-    				if(biome == Biomes.NETHER) {
-    					// Ore generation
-    					ConfiguredPlacement<CountRangeConfig> customConfig = Placement.COUNT_RANGE
-    							.configure(new CountRangeConfig(3, 4, 4, 5));
-    					biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE
-    							.withConfiguration(new OreFeatureConfig(FillerBlockType.NETHERRACK, DeferredRegistryHandler.MOLTEN_ORE.get().getDefaultState(), 3))
-    							.withPlacement(customConfig));
-    				}
-    				if(biome == Biomes.PLAINS) {
-    					// Structure generation
-    					biome.addStructure(ABANDONED_FACTORY.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG));
-    					biome.addFeature(GenerationStage.Decoration.SURFACE_STRUCTURES, ABANDONED_FACTORY.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG).withPlacement(Placement.NOPE.configure(IPlacementConfig.NO_PLACEMENT_CONFIG)));
-    				}
-    				if(biome == Biomes.SWAMP) {
-    					// Structure generation
-    					biome.addStructure(ABANDONED_FACTORY.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG));
-    					biome.addFeature(GenerationStage.Decoration.SURFACE_STRUCTURES, ABANDONED_FACTORY.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG).withPlacement(Placement.NOPE.configure(IPlacementConfig.NO_PLACEMENT_CONFIG)));
-    				}
-    				if(biome == Biomes.SAVANNA) {
-    					// Structure generation
-    					biome.addStructure(ABANDONED_FACTORY.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG));
-    					biome.addFeature(GenerationStage.Decoration.SURFACE_STRUCTURES, ABANDONED_FACTORY.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG).withPlacement(Placement.NOPE.configure(IPlacementConfig.NO_PLACEMENT_CONFIG)));
-    				}
-    				// Ores that should generate in all biomes should go here
-    				// Copper Ore
-    				ConfiguredPlacement<CountRangeConfig> customConfig = Placement.COUNT_RANGE
-							.configure(new CountRangeConfig(9, 12, 7, 60));
-					biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE
-							.withConfiguration(new OreFeatureConfig(FillerBlockType.NATURAL_STONE, DeferredRegistryHandler.COPPER_ORE.get().getDefaultState(), 9))
-							.withPlacement(customConfig));
-    			}
-    		}
-    	});
+
     }
     
 
--- src/main/java/com/anonymoushacker1279/immersiveweapons/util/CustomArmorMaterials.java
+++ src/main/java/com/anonymoushacker1279/immersiveweapons/util/CustomArmorMaterials.java
@@ -15,10 +15,10 @@ public enum CustomArmorMaterials implements IArmorMaterial {
 
 	 MOLTEN("molten", 45, new int[]{5, 6, 9, 4}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.5F, () -> {
 		 return Ingredient.fromItems(DeferredRegistryHandler.MOLTEN_PLATE.get()); 
-	 }),
+	 }, 0.12F),
 	 COPPER("copper", 25, new int[]{1, 4, 5, 1}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, () -> {
 		 return Ingredient.fromItems(DeferredRegistryHandler.COPPER_INGOT.get());
-	 });
+	 }, 0.0F);
 	 
 	   static final int[] MAX_DAMAGE_ARRAY = new int[]{13, 15, 16, 11};
 	   private final String name;
@@ -28,8 +28,9 @@ public enum CustomArmorMaterials implements IArmorMaterial {
 	   private final SoundEvent soundEvent;
 	   private final float toughness;
 	   private final LazyValue<Ingredient> repairMaterial;
+	   private final float knockbackResistance;
 
-	  CustomArmorMaterials(String nameIn, int maxDamageFactorIn, int[] damageReductionAmountsIn, int enchantabilityIn, SoundEvent equipSoundIn, float toughnessIn, Supplier<Ingredient> repairMaterialSupplier) {
+	  CustomArmorMaterials(String nameIn, int maxDamageFactorIn, int[] damageReductionAmountsIn, int enchantabilityIn, SoundEvent equipSoundIn, float toughnessIn, Supplier<Ingredient> repairMaterialSupplier, float knockbackResistance) {
 	      this.name = nameIn;
 	      this.maxDamageFactor = maxDamageFactorIn;
 	      this.damageReductionAmountArray = damageReductionAmountsIn;
@@ -37,6 +38,7 @@ public enum CustomArmorMaterials implements IArmorMaterial {
 	      this.soundEvent = equipSoundIn;
 	      this.toughness = toughnessIn;
 	      this.repairMaterial = new LazyValue<>(repairMaterialSupplier);
+	      this.knockbackResistance = knockbackResistance;
 	   }
 
 	   public int getDurability(EquipmentSlotType slotIn) {
@@ -67,4 +69,9 @@ public enum CustomArmorMaterials implements IArmorMaterial {
 	   public float getToughness() {
 	      return this.toughness;
 	   }
+
+	@Override
+	public float getKnockbackResistance() {
+		return this.knockbackResistance;
+	}
 }
