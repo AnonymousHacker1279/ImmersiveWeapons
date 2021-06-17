a53434d44800305e0a07182cd951ffd5c55a1010
Anonymous Hacker <mcgamewarrior@gmail.com>
2021-06-16 22:10:35 -0400

    Fix special armor effects failing on dedicated server
    Properly utilize packets for special armor effects. Fix errors relating to checking keybinds on the server.

--- src/main/java/com/anonymoushacker1279/immersiveweapons/util/PacketHandler.java
+++ src/main/java/com/anonymoushacker1279/immersiveweapons/util/PacketHandler.java
@@ -1,6 +1,12 @@
 package com.anonymoushacker1279.immersiveweapons.util;
 
 import com.anonymoushacker1279.immersiveweapons.ImmersiveWeapons;
+import com.anonymoushacker1279.immersiveweapons.entity.projectile.MolotovEntity.MolotovEntityPacketHandler;
+import com.anonymoushacker1279.immersiveweapons.entity.projectile.SmokeBombEntity.SmokeBombEntityPacketHandler;
+import com.anonymoushacker1279.immersiveweapons.item.CobaltArmorItem.CobaltArmorItemPacketHandler;
+import com.anonymoushacker1279.immersiveweapons.item.TeslaArmorItem.TeslaArmorItemPacketHandler;
+import com.anonymoushacker1279.immersiveweapons.item.VentusArmorItem.VentusArmorItemPacketHandler;
+import com.anonymoushacker1279.immersiveweapons.tileentity.PanicAlarmTileEntity.PanicAlarmPacketHandler;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.network.NetworkRegistry;
 import net.minecraftforge.fml.network.simple.SimpleChannel;
@@ -14,4 +20,44 @@ public class PacketHandler {
 			PROTOCOL_VERSION::equals,
 			PROTOCOL_VERSION::equals
 	);
+
+	public static void registerPackets() {
+		int networkId = 0;
+		PacketHandler.INSTANCE.registerMessage(networkId++,
+				PanicAlarmPacketHandler.class,
+				PanicAlarmPacketHandler::encode,
+				PanicAlarmPacketHandler::decode,
+				PanicAlarmPacketHandler::handle
+		);
+		PacketHandler.INSTANCE.registerMessage(networkId++,
+				SmokeBombEntityPacketHandler.class,
+				SmokeBombEntityPacketHandler::encode,
+				SmokeBombEntityPacketHandler::decode,
+				SmokeBombEntityPacketHandler::handle
+		);
+		PacketHandler.INSTANCE.registerMessage(networkId++,
+				MolotovEntityPacketHandler.class,
+				MolotovEntityPacketHandler::encode,
+				MolotovEntityPacketHandler::decode,
+				MolotovEntityPacketHandler::handle
+		);
+		PacketHandler.INSTANCE.registerMessage(networkId++,
+				CobaltArmorItemPacketHandler.class,
+				CobaltArmorItemPacketHandler::encode,
+				CobaltArmorItemPacketHandler::decode,
+				CobaltArmorItemPacketHandler::handle
+		);
+		PacketHandler.INSTANCE.registerMessage(networkId++,
+				TeslaArmorItemPacketHandler.class,
+				TeslaArmorItemPacketHandler::encode,
+				TeslaArmorItemPacketHandler::decode,
+				TeslaArmorItemPacketHandler::handle
+		);
+		PacketHandler.INSTANCE.registerMessage(networkId++,
+				VentusArmorItemPacketHandler.class,
+				VentusArmorItemPacketHandler::encode,
+				VentusArmorItemPacketHandler::decode,
+				VentusArmorItemPacketHandler::handle
+		);
+	}
 }
