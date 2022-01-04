8787bdc17b7fe149baee57efbcb10516e5ef8b80
Anonymous Hacker <mcgamewarrior@gmail.com>
2022-01-03 21:50:58 -0500

    Remove workaround for custom wood types
    Remove a workaround for custom wood types that would not accept non-vanilla namespaces, as it does not seem to be needed anymore

--- src/main/java/com/anonymoushacker1279/immersiveweapons/util/CustomWoodTypes.java
+++ src/main/java/com/anonymoushacker1279/immersiveweapons/util/CustomWoodTypes.java
@@ -1,8 +1,10 @@
 package com.anonymoushacker1279.immersiveweapons.util;
 
+import com.anonymoushacker1279.immersiveweapons.ImmersiveWeapons;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.state.properties.WoodType;
 
 public class CustomWoodTypes {
 
-	public static final WoodType BURNED_OAK = WoodType.create("immersiveweapons_burned_oak");
+	public static final WoodType BURNED_OAK = WoodType.create(new ResourceLocation(ImmersiveWeapons.MOD_ID, "burned_oak").toString());
 }
