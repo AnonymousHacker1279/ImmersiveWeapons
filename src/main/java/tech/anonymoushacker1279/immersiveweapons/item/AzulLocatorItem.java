ace2f41e3cf746651eb584f787af6720205c10e5
Nicholas Smit <mcgamewarrior@gmail.com>
2023-03-08 22:22:02 -0500

    Allow Azul Locator to teleport between dimensions
    Update the Azul Locator to teleport the player between dimensions so they always end up at the proper spawn point.

--- src/main/java/tech/anonymoushacker1279/immersiveweapons/item/AzulLocatorItem.java
+++ src/main/java/tech/anonymoushacker1279/immersiveweapons/item/AzulLocatorItem.java
@@ -3,6 +3,7 @@ package tech.anonymoushacker1279.immersiveweapons.item;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
@@ -11,6 +12,8 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+
+import java.util.Objects;
 
 public class AzulLocatorItem extends Item {
 
@@ -42,15 +45,19 @@ public class AzulLocatorItem extends Item {
 				if (!level.isClientSide) {
 					ServerPlayer serverPlayer = (ServerPlayer) player;
 					BlockPos spawnPos = serverPlayer.getRespawnPosition();
+					ServerLevel spawnLevel = Objects.requireNonNull(serverPlayer.getServer()).getLevel(serverPlayer.getRespawnDimension());
 
 					if (spawnPos == null) {
 						player.displayClientMessage(Component.translatable("immersiveweapons.item.azul_locator.no_spawn")
 								.withStyle(ChatFormatting.RED), true);
 						player.getCooldowns().addCooldown(this, 60);
 					} else {
-						player.teleportTo(spawnPos.getX() + 0.5f, spawnPos.getY(), spawnPos.getZ() + 0.5f);
-						player.displayClientMessage(Component.translatable("immersiveweapons.item.azul_locator.teleported")
-								.withStyle(ChatFormatting.GREEN), true);
+						if (spawnLevel != null) {
+							serverPlayer.teleportTo(spawnLevel, spawnPos.getX() + 0.5f, spawnPos.getY(), spawnPos.getZ() + 0.5f, player.getYRot(), player.getXRot());
+							player.teleportTo(spawnPos.getX() + 0.5f, spawnPos.getY(), spawnPos.getZ() + 0.5f);
+							player.displayClientMessage(Component.translatable("immersiveweapons.item.azul_locator.teleported")
+									.withStyle(ChatFormatting.GREEN), true);
+						}
 
 						if (!player.isCreative()) {
 							stack.hurtAndBreak(1, player, (entity) -> entity.broadcastBreakEvent(player.getUsedItemHand()));
