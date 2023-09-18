24f285c0f02f7aa2693f5ebdc8d7a5842bb0c1e7
Nicholas Smit <mcgamewarrior@gmail.com>
2023-09-18 15:56:13 -0400

    Fix potential server crash
    Fix a potential crash regarding bullet impact particles on dedicated servers in some scenarios.

--- src/main/java/tech/anonymoushacker1279/immersiveweapons/client/particle/bullet_impact/BulletImpactParticle.java
+++ src/main/java/tech/anonymoushacker1279/immersiveweapons/client/particle/bullet_impact/BulletImpactParticle.java
@@ -1,8 +1,15 @@
 package tech.anonymoushacker1279.immersiveweapons.client.particle.bullet_impact;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.*;
+import net.minecraft.client.renderer.block.BlockModelShaper;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.util.Mth;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.data.ModelData;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3f;
 import tech.anonymoushacker1279.immersiveweapons.util.GeneralUtilities;
@@ -23,7 +30,7 @@ public class BulletImpactParticle extends TextureSheetParticle {
 		                               double pXSpeed, double pYSpeed, double pZSpeed) {
 
 			if (sprites != null) {
-				return new BulletImpactParticle(pLevel, pX, pY, pZ, pXSpeed, pYSpeed, pZSpeed, sprites, pType.getColor());
+				return new BulletImpactParticle(pLevel, pX, pY, pZ, pXSpeed, pYSpeed, pZSpeed, sprites, pType.getBlockID());
 			}
 			return null;
 		}
@@ -31,9 +38,11 @@ public class BulletImpactParticle extends TextureSheetParticle {
 
 	protected BulletImpactParticle(ClientLevel level, double x, double y, double z,
 	                               double xSpeed, double ySpeed,
-	                               double zSpeed, SpriteSet spriteSet, Vector3f color) {
+	                               double zSpeed, SpriteSet spriteSet, int blockID) {
 
 		super(level, x, y, z, 0.0D, 0.0D, 0.0D);
+
+		Vector3f color = getColorFromBlock(blockID);
 
 		friction = 0.96F;
 		gravity = (float) 0.175;
@@ -59,6 +68,20 @@ public class BulletImpactParticle extends TextureSheetParticle {
 
 	protected float randomizeColor(float color, float multiplier) {
 		return (random.nextFloat() * 0.05F + 0.95F) * color * multiplier;
+	}
+
+	protected Vector3f getColorFromBlock(int blockID) {
+		Minecraft minecraft = Minecraft.getInstance();
+		BlockRenderDispatcher blockRendererDispatcher = minecraft.getBlockRenderer();
+		BlockModelShaper blockModelShapes = blockRendererDispatcher.getBlockModelShaper();
+		BakedModel blockModel = blockModelShapes.getBlockModel(Block.stateById(blockID));
+		TextureAtlasSprite textureAtlasSprite = blockModel.getParticleIcon(ModelData.EMPTY);
+
+		int pixelABGR = textureAtlasSprite.getPixelRGBA(0, 0, 0);
+		int r = pixelABGR & 0xff;
+		int g = pixelABGR >> 8 & 0xff;
+		int b = pixelABGR >> 16 & 0xff;
+		return new Vector3f(r / 255f, g / 255f, b / 255f);
 	}
 
 	@Override
