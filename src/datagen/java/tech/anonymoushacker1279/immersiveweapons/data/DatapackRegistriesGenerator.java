508ba8be68d0bc1dc74a2e3c10230f0856bc8d7f
Nicholas Smit <mcgamewarrior@gmail.com>
2024-02-05 20:28:53 -0500

    Datagen structure processor lists
    Setup data generators for structure processor lists.

--- src/datagen/java/tech/anonymoushacker1279/immersiveweapons/data/DatapackRegistriesGenerator.java
+++ src/datagen/java/tech/anonymoushacker1279/immersiveweapons/data/DatapackRegistriesGenerator.java
@@ -12,6 +12,7 @@ import tech.anonymoushacker1279.immersiveweapons.data.damage_types.DamageTypesGe
 import tech.anonymoushacker1279.immersiveweapons.data.dimensions.IWDimensions;
 import tech.anonymoushacker1279.immersiveweapons.data.features.*;
 import tech.anonymoushacker1279.immersiveweapons.data.modifiers.IWBiomeModifiers;
+import tech.anonymoushacker1279.immersiveweapons.data.structures.StructureProcessorListGenerator;
 
 import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
@@ -25,7 +26,8 @@ public class DatapackRegistriesGenerator extends DatapackBuiltinEntriesProvider 
 			.add(Registries.DIMENSION_TYPE, IWDimensions::bootstrapDimensionType)
 			.add(Registries.CONFIGURED_CARVER, IWConfiguredCarvers::bootstrap)
 			.add(Keys.BIOME_MODIFIERS, IWBiomeModifiers::bootstrap)
-			.add(Registries.DAMAGE_TYPE, DamageTypesGenerator::bootstrap);
+			.add(Registries.DAMAGE_TYPE, DamageTypesGenerator::bootstrap)
+			.add(Registries.PROCESSOR_LIST, StructureProcessorListGenerator::bootstrap);
 
 	public DatapackRegistriesGenerator(PackOutput output, CompletableFuture<Provider> registries) {
 		super(output, registries, BUILDER, Collections.singleton(ImmersiveWeapons.MOD_ID));
---
+++ src/datagen/java/tech/anonymoushacker1279/immersiveweapons/data/structures/StructureProcessorListGenerator.java
@@ -0,0 +1,41 @@
+package tech.anonymoushacker1279.immersiveweapons.data.structures;
+
+import com.google.common.collect.ImmutableList;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.structure.templatesystem.*;
+import tech.anonymoushacker1279.immersiveweapons.ImmersiveWeapons;
+import tech.anonymoushacker1279.immersiveweapons.init.BlockRegistry;
+
+import java.util.List;
+
+public class StructureProcessorListGenerator {
+
+	public static final ResourceKey<StructureProcessorList> RUST_50_PERCENT = createKey("rust_50_percent");
+	public static final ResourceKey<StructureProcessorList> RUST_70_PERCENT = createKey("rust_70_percent");
+
+	private static ResourceKey<StructureProcessorList> createKey(String name) {
+		return ResourceKey.create(Registries.PROCESSOR_LIST, new ResourceLocation(ImmersiveWeapons.MOD_ID, name));
+	}
+
+	public static void bootstrap(BootstapContext<StructureProcessorList> context) {
+		register(context, RUST_50_PERCENT, ImmutableList.of(
+				new RuleProcessor(ImmutableList.of(
+						new ProcessorRule(new RandomBlockMatchTest(Blocks.IRON_BLOCK, 0.5f), AlwaysTrueTest.INSTANCE, BlockRegistry.RUSTED_IRON_BLOCK.get().defaultBlockState())
+				))
+		));
+		
+		register(context, RUST_70_PERCENT, ImmutableList.of(
+				new RuleProcessor(ImmutableList.of(
+						new ProcessorRule(new RandomBlockMatchTest(Blocks.IRON_BLOCK, 0.7f), AlwaysTrueTest.INSTANCE, BlockRegistry.RUSTED_IRON_BLOCK.get().defaultBlockState())
+				))
+		));
+	}
+
+	protected static void register(BootstapContext<StructureProcessorList> context, ResourceKey<StructureProcessorList> key, List<StructureProcessor> processorList) {
+		context.register(key, new StructureProcessorList(processorList));
+	}
+}
